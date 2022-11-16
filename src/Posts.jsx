7dee980f34882e314fa89430db70f0c522a8541d
Sherry90 <hhlsdkm@gmail.com>
2022-11-16 23:55:23 +0900

    useQuery

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,21 +1,24 @@
 import { useState } from "react";
+import { useQuery } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+const asyncfetchPosts = async() => {
   const response = await fetch(
     "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
   );
   return response.json();
 }
 
-export function Posts() {
+export const Posts = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const data = [];
+  const { data } = useQuery("posts", asyncfetchPosts);
+
+  if(!data) return <div />
 
   return (
     <>
